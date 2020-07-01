fccef709c01e992637e492b6e0cda5089385ab7a
Richard Doe <richard.doe@rwdit.net>
2020-07-01 14:20:58 +0100

    POC: index included sys.id in Redis

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const redis = require('redis');
 
+const flatten = require('lodash/flatten');
+const compact = require('lodash/compact');
+const uniq = require('lodash/uniq');
+
 const loadRuntimeConfiguration = () => {
   let rc = {};
 
@@ -37,6 +41,7 @@ const apicacheOptions = {
   debug: Number(process.env.ENABLE_APICACHE_DEBUG)
 };
 
+let redisClient;
 if (process.env.REDIS_URL) {
   const redisOptions = {
     url: process.env.REDIS_URL
@@ -46,7 +51,8 @@ if (process.env.REDIS_URL) {
       ca: [Buffer.from(process.env.REDIS_TLS_CA, 'base64')]
     };
   }
-  apicacheOptions.redisClient = redis.createClient(redisOptions);
+  redisClient = redis.createClient(redisOptions);
+  apicacheOptions.redisClient = redisClient;
 }
 
 const app = express();
@@ -64,6 +70,51 @@ app.get('/', (req, res) => {
 const onlyStatus200 = (req, res) => res.statusCode === 200;
 const cacheSuccesses = cache(process.env.CACHE_DURATION || '5 minutes', onlyStatus200);
 
+// Gathers all sys.id properties at any depth of an object
+const getSysIds = (data) => {
+  let sysIds = [];
+  if (Array.isArray(data)) {
+    sysIds = data.map(getSysIds);
+  } else if (typeof data === 'object') {
+    for (const key in data) {
+      if (key === 'sys' && data.sys.id) {
+        sysIds = sysIds.concat(data.sys.id);
+      } else {
+        sysIds = sysIds.concat(getSysIds(data[key]));
+      }
+    }
+  }
+  return compact(uniq(flatten(sysIds)));
+};
+
+// Write sys.ids in proxied response to Redis
+//
+// TODO: add an app endpoint with authorisation to clear all relevant cache
+//       entries when a given sys id changes, to be called by a webhook.
+// TODO: account somehow for creation/publication of new content, e.g. for
+//       content listing queries, perhaps by also indexing the content types
+//       included in responses
+const onProxyRes = (proxyRes, req) => {
+  // TODO: only do this if it's JSON, and if redis client is configured
+  let rawData = '';
+  proxyRes.on('data', (chunk) => {
+    rawData += chunk;
+  });
+  proxyRes.on('end', () => {
+    const parsedData = JSON.parse(rawData);
+    const sysIds = getSysIds(parsedData);
+
+    for (const id of sysIds) {
+      const redisKey = `/index/${id}`;
+      redisClient.get(redisKey, (err, res) => {
+        const index = (res ? JSON.parse(res) : []).concat(req.url);
+        const redisValue = JSON.stringify(index);
+        redisClient.set(redisKey, redisValue);
+      });
+    }
+  });
+};
+
 const rc = loadRuntimeConfiguration();
 for (const path in rc) {
   const target = rc[path];
@@ -72,7 +123,8 @@ for (const path in rc) {
     cacheSuccesses,
     createProxyMiddleware({
       target,
-      changeOrigin: true
+      changeOrigin: true,
+      onProxyRes
     })
   );
 }
